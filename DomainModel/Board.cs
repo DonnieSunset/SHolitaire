7dd8b08d0347d58d23bc1a7c3ea848ea628df1fe
Randall Flagg <flagg1980@outlook.com>
2018-05-27 16:01:44 +0200

    Initial Checkin

---
+++ DomainModel/Board.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SHolitaire.DomainModel
+{
+    public class Board
+    {
+        List<Card> FieldDeck = new List<Card>();
+
+        List<List<Card>> FieldStack = new List<List<Card>>();
+
+        List<List<Card>> FieldDone = new List<List<Card>>();
+    }
+}
---
+++ DomainModel/Cards.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SHolitaire.DomainModel
+{
+    public enum Rank
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+
+    public enum Suit
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    }
+
+    public class Card
+    {
+        Rank rank;
+        Suit suit;
+
+        public Card(Rank rank, Suit suit)
+        {
+            this.rank = rank;
+            this.suit = suit;
+        }
+
+        public bool Opened { get; set; }
+    }
+
+    public abstract class Deck
+    {
+        public List<Card> Cards { get; protected set; }
+    }
+
+    public class ClassicDeck : Deck
+    {
+        public ClassicDeck()
+        {
+            Cards = new List<Card>();
+
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    Cards.Add(new Card(rank, suit));
+                }
+            }
+        }
+    }
+
+    public class SpiderDeck : Deck
+    {
+        public SpiderDeck()
+        {
+            Cards = new List<Card>();
+
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    Cards.Add(new Card(rank, suit));
+                    Cards.Add(new Card(rank, suit));
+                }
+            }
+        }
+    }
+}
